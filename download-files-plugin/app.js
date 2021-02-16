53407d6fa554824dc40eb8ed71b85b86eea4e1bc
aleksey-13 <alekseymagada@gmail.com>
2021-02-16 16:14:17 +0200

    add delete img. add upload on server

--- download-files-plugin/app.js
+++ download-files-plugin/app.js
@@ -1,6 +1,54 @@
+import firebase from 'firebase/app'
+import 'firebase/storage'
 import { upload } from './upload'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyBl5AS71axae_v816LjNhYi5avQEzcxsak',
+    authDomain: 'upload-files-91645.firebaseapp.com',
+    projectId: 'upload-files-91645',
+    storageBucket: 'upload-files-91645.appspot.com',
+    messagingSenderId: '1074153524015',
+    appId: '1:1074153524015:web:160fe3b90cb10e6ae22fda',
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+const storage = firebase.storage()
 
 upload('#file', {
     multi: true,
     accept: ['.png', '.jpg', 'jpeg', '.gif'],
+    onUpload(files, blocks) {
+        files.forEach((file, index) => {
+            const ref = storage.ref(`images/${file.name}`)
+            const task = ref.put(file)
+
+            task.on(
+                'state_changed',
+                (snapshot) => {
+                    const precentage =
+                        (
+                            (snapshot.bytesTransferred / snapshot.totalBytes) *
+                            100
+                        ).toFixed(0) + '%'
+
+                    const block = blocks[index].querySelector(
+                        '.preview-info-progress'
+                    )
+
+                    block.textContent = precentage
+                    block.style.width = precentage
+                },
+                (error) => {
+                    console.log(error)
+                },
+                () => {
+                    task.snapshot.ref
+                        .getDownloadURL()
+                        .then((url) => console.log('url', url))
+                }
+            )
+        })
+    },
 })
--- download-files-plugin/upload.js
+++ download-files-plugin/upload.js
@@ -1,12 +1,22 @@
 export function upload(selector, options = {}) {
+    let files = []
+    const onUpload = options.onUpload || noop
     const input = document.querySelector(selector)
 
-    const preview = document.createElement('div')
-    preview.classList.add('preview')
+    const preview = createElement({ tag: 'div', className: ['preview'] })
 
-    const open = document.createElement('button')
-    open.classList.add('btn')
-    open.textContent = 'Открыть'
+    const open = createElement({
+        tag: 'button',
+        content: 'Открыть',
+        className: ['btn'],
+    })
+
+    const upload = createElement({
+        tag: 'button',
+        content: 'Загрузить',
+        className: ['btn', 'primary'],
+    })
+    upload.style.display = 'none'
 
     if (options.multi) {
         input.setAttribute('multiple', true)
@@ -17,6 +27,7 @@ export function upload(selector, options = {}) {
     }
 
     input.insertAdjacentElement('afterend', preview)
+    input.insertAdjacentElement('afterend', upload)
     input.insertAdjacentElement('afterend', open)
 
     const triggerInput = () => input.click()
@@ -26,8 +37,10 @@ export function upload(selector, options = {}) {
             return
         }
 
-        const files = Array.from(event.target.files)
+        files = Array.from(event.target.files)
         preview.innerHTML = ''
+        upload.style.display = 'inline'
+
         files.forEach((file) => {
             if (!file.type.match('image')) {
                 return
@@ -39,8 +52,10 @@ export function upload(selector, options = {}) {
                 preview.insertAdjacentHTML(
                     'afterbegin',
                     `
-                    <div class="preview-image" data-id="${file.name}">
-                        <div class="preview-remove">&times;</div>
+                    <div class="preview-image">
+                        <div class="preview-remove" data-name="${
+                            file.name
+                        }">&times;</div>
                         <img src="${reader.result}" alt="${file.name}" />
                         <div class="preview-info">
                             <span>${file.name}</span>
@@ -55,8 +70,50 @@ export function upload(selector, options = {}) {
         })
     }
 
+    const removeHandler = (event) => {
+        if (!event.target.dataset.name) {
+            return
+        }
+
+        const { name } = event.target.dataset
+
+        files = files.filter((file) => file.name !== name)
+
+        const block = preview
+            .querySelector(`[data-name="${name}"`)
+            .closest('.preview-image')
+
+        block.classList.add('remove')
+
+        setTimeout(() => block.remove(), 300)
+
+        if (!files.length) {
+            upload.style.display = 'none'
+        }
+    }
+
+    const clearPreview = (node) => {
+        node.style.bottom = 0
+        node.innerHTML = `<div class="preview-info-progress"></div>`
+
+        return node
+    }
+
+    const uploadHandler = () => {
+        preview
+            .querySelectorAll('.preview-remove')
+            .forEach((node) => node.remove())
+        const preivewInfo = Array.from(
+            preview.querySelectorAll('.preview-info')
+        ).map(clearPreview)
+
+        onUpload(files, preivewInfo)
+    }
+
     open.addEventListener('click', triggerInput)
     input.addEventListener('change', changeHandler)
+    preview.addEventListener('click', removeHandler)
+    upload.addEventListener('click', uploadHandler)
 }
 
 function bytesToSize(bytes) {
@@ -65,3 +122,20 @@ function bytesToSize(bytes) {
     const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
     return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i]
 }
+
+function createElement(options) {
+    const { tag, content, className = [] } = options
+    const el = document.createElement(tag)
+
+    if (content) {
+        el.textContent = content
+    }
+
+    if (className) {
+        el.classList.add(...className)
+    }
+
+    return el
+}
+
+function noop() {}
